9e7939eaf6549d053ee8148b1bd9181c76322c0b
aaronmcgill <aaron.garcia@mail.mcgill.ca>
2023-06-18 17:52:17 -0400

    iinit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Pedorro todo</h1>
+    <div className="container">
+      <form>
+        <h2>Add Item</h2>
+        <input type="text" id="title" placeholder="Title" className="form-control" autocomplete="off"/>
+        <button>Add</button>
+      </form>
+
+      <div className="items">
+        <h2>My Items</h2>
+      </div>
+    </div>
+
     </div>
   );
 }
